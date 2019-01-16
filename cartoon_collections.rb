2c6d9d84c25dab3b53cf01ac427e9d0ec9521629
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-16 00:12:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.each {|x|}
+  calls_long.each do |x|
     if x.size > 4 
      return TRUE
   end
