8895247dbd30e05c9be078a150917a87a70505e8
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-16 00:55:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,6 @@ def long_planeteer_calls(calls_long)
   return FALSE
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array,cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
