de2f59c9a9eae994a549b292e971071e62aa8dc9
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-16 01:13:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,14 +20,11 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(array)
-  array.each do |x|
+  cheese_types = ["cheddar", "gouda", "camembert"]
     if x.include?(cheese_types)
       return x
     else
       return NIL
     end
   end
-      
-  cheese_types = ["cheddar", "gouda", "camembert"]
- 
 end
