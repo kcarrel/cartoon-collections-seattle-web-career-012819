4afb2b43247b4d34c761f79a9ebfbd26766b8d40
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-16 01:37:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |x|
     if x.include? (cheese_types(x))
-      return x 
+      return array(x)
     end
   end
 end
